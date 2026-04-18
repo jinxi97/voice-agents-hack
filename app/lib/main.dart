c4ab8137a77aa7d1b623d33fbbe9f4c7dae8b611
Jason Jin <jinxi_1997@outlook.com>
2026-04-18 14:30:52 -0700

    flutter

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,121 +1,195 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+const String appId = String.fromEnvironment('AGORA_APP_ID');
+const String token = String.fromEnvironment('AGORA_TOKEN');
+const String channelName = String.fromEnvironment('AGORA_CHANNEL');
 
 void main() {
-  runApp(const MyApp());
+  runApp(const VideoCallApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class VideoCallApp extends StatelessWidget {
+  const VideoCallApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Agora Video Call',
+      theme: ThemeData.dark(useMaterial3: true),
+      home: const CallPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CallPage extends StatefulWidget {
+  const CallPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CallPage> createState() => _CallPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CallPageState extends State<CallPage> {
+  late final RtcEngine _engine;
+  bool _joined = false;
+  int? _remoteUid;
+  bool _muted = false;
+  bool _cameraOff = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initAgora();
+  }
+
+  Future<void> _initAgora() async {
+    await [Permission.microphone, Permission.camera].request();
+
+    _engine = createAgoraRtcEngine();
+    await _engine.initialize(const RtcEngineContext(
+      appId: appId,
+      channelProfile: ChannelProfileType.channelProfileCommunication,
+    ));
+
+    _engine.registerEventHandler(RtcEngineEventHandler(
+      onJoinChannelSuccess: (connection, elapsed) {
+        if (mounted) setState(() => _joined = true);
+      },
+      onUserJoined: (connection, remoteUid, elapsed) {
+        if (mounted) setState(() => _remoteUid = remoteUid);
+      },
+      onUserOffline: (connection, remoteUid, reason) {
+        if (mounted && _remoteUid == remoteUid) {
+          setState(() => _remoteUid = null);
+        }
+      },
+    ));
+
+    await _engine.enableVideo();
+    await _engine.startPreview();
+    await _engine.joinChannel(
+      token: token,
+      channelId: channelName,
+      uid: 0,
+      options: const ChannelMediaOptions(
+        clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        channelProfile: ChannelProfileType.channelProfileCommunication,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _engine.leaveChannel();
+    _engine.release();
+    super.dispose();
+  }
+
+  Widget _remoteView() {
+    if (_remoteUid == null) {
+      return const Center(
+        child: Text('Waiting for another user to join…',
+            style: TextStyle(color: Colors.white70)),
+      );
+    }
+    return AgoraVideoView(
+      controller: VideoViewController.remote(
+        rtcEngine: _engine,
+        canvas: VideoCanvas(uid: _remoteUid),
+        connection: const RtcConnection(channelId: channelName),
+      ),
+    );
+  }
+
+  Widget _localView() {
+    if (!_joined) return const ColoredBox(color: Colors.black);
+    return AgoraVideoView(
+      controller: VideoViewController(
+        rtcEngine: _engine,
+        canvas: const VideoCanvas(uid: 0),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Stack(
+        children: [
+          Positioned.fill(child: _remoteView()),
+          Positioned(
+            top: 40,
+            right: 16,
+            width: 120,
+            height: 160,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: _localView(),
             ),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _CircleButton(
+                  icon: _muted ? Icons.mic_off : Icons.mic,
+                  color: _muted ? Colors.red : Colors.white24,
+                  onTap: () async {
+                    await _engine.muteLocalAudioStream(!_muted);
+                    setState(() => _muted = !_muted);
+                  },
+                ),
+                _CircleButton(
+                  icon: Icons.call_end,
+                  color: Colors.red,
+                  onTap: () => Navigator.of(context).maybePop(),
+                ),
+                _CircleButton(
+                  icon: _cameraOff ? Icons.videocam_off : Icons.videocam,
+                  color: _cameraOff ? Colors.red : Colors.white24,
+                  onTap: () async {
+                    await _engine.muteLocalVideoStream(!_cameraOff);
+                    setState(() => _cameraOff = !_cameraOff);
+                  },
+                ),
+                _CircleButton(
+                  icon: Icons.cameraswitch,
+                  color: Colors.white24,
+                  onTap: () => _engine.switchCamera(),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class _CircleButton extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  final VoidCallback onTap;
+
+  const _CircleButton({
+    required this.icon,
+    required this.color,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 56,
+        height: 56,
+        decoration: BoxDecoration(color: color, shape: BoxShape.circle),
+        child: Icon(icon, color: Colors.white),
       ),
     );
   }
